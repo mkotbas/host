e27834b3264f8f381b1f518bf39eaf48e6f46975
mkotbas <71524519+mkotbas@users.noreply.github.com>
2025-11-02 14:14:03 +0300

    eee

--- fide/vite.config.js
+++ fide/vite.config.js
@@ -5,18 +5,14 @@ import { fileURLToPath } from 'url'
 const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 export default defineConfig({
-   --- YENİ EKLENEN SATIR ---
-   Dosya yollarını göreceli ('.assets' gibi) yapar.
-  base '.', 
-   --- YENİ EKLENEN SATIR BİTTİ ---
-
-  root __dirname,
-  build {
-    outDir 'dist',
-    rollupOptions {
-      input {
-        main resolve(__dirname, 'index.html'),
-        admin resolve(__dirname, 'adminadmin.html')
+  base: './',
+  root: __dirname,
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'admin/admin.html')
       }
     }
   }
