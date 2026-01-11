4763aef2af4dde895db6f51504cd92f7447450cc
mkotbas <71524519+mkotbas@users.noreply.github.com>
2026-01-12 01:33:09 +0300

    ddd

--- Bayi Mesaj Sistemi/sehir-mesaj.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-:: Klasörü Python dosyasının olduğu yere odaklar
-cd /d %~dp0
-:: Python dosyasını çalıştırır
-python sehir-mesaj.py
-pause
--- Bayi Mesaj Sistemi/çalıştır.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-python gonder.py
-pause
